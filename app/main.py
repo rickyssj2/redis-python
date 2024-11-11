ade404b2cd11531b9cbd9631ed3321190c0a5e46
Aryan Pandey <aryanpandey048@gmail.com>
2024-11-11 11:09:47 -0800

    Added support to keep connection alive and accept multiple PING requests from a connected client

--- app/main.py
+++ app/main.py
@@ -6,9 +6,15 @@ def main():
         while True:
             conn, addr = server.accept()
             print(f"addr: {addr}")
-            request = conn.recv(100)
-            print(request)
-            conn.send(b"+PONG\r\n")
+            request = b""
+            with conn:
+                while True:
+                    request = conn.recv(100)
+                    if request.decode('utf-8').endswith("exit\n"):
+                        break
+                    print(request)
+                    conn.send(b"+PONG\r\n")
+            print("Client exited!\r\nConnection Closed.")
 
 if __name__ == "__main__":
     main()
