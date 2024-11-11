502dc257260d4ab3b661059dc1056ba8990d68b0
Aryan Pandey <aryanpandey048@gmail.com>
2024-11-11 10:10:35 -0800

    Implemented simple PING command

--- app/main.py
+++ app/main.py
@@ -2,9 +2,13 @@ import socket  # noqa: F401
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    with socket.create_server(("localhost", 6379), reuse_port=True, ) as server:
+        while True:
+            conn, addr = server.accept()
+            print(f"addr: {addr}")
+            request = conn.recv(100)
+            print(request)
+            conn.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
